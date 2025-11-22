a93287367fde48ba16b9fd2f4e73641e00360226
Alexander Cyon <alex.cyon@gmail.com>
2025-11-22 07:35:26 +0100

    Concurrency adjustments, SPM: platforms, Demo: use LOCAL Package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// later change swift-tools-version to 6.2
+
 import PackageDescription
 
 let package = Package(
     name: "DfnsSdk",
+	platforms: [
+		.macOS(.v10_15),
+		.iOS(.v15),
+		.watchOS(.v6),
+		.tvOS(.v13)
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
